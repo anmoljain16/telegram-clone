698e12bbb390eed1057b132b0b5550e058f38ae2
anmoljain16 <ajanmoldosi@gmail.com>
2024-07-15 21:06:27 +0530

    fixing ChatWindow for dark mode

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-// pages/page.js
 "use client"
 import { useState } from 'react';
 import ChatList from '../components/ChatList';
 import ChatWindow from '../components/ChatWindow';
 import Drawer from '../components/Drawer';
+import Slide from "@mui/material/Slide";
+
 
 const Home = () => {
     const [selectedChatId, setSelectedChatId] = useState(null);
@@ -18,15 +19,13 @@ const Home = () => {
     return (
         <div className={`flex flex-row h-screen ${darkMode ? 'dark' : ''}`}>
             <div className={`flex items-start justify-between border-b ${darkMode ? 'bg-slate-800 text-white border-gray-700' : 'bg-slate-700 text-slate-400 border-gray-300'}`} >
-                <button className="m-4  " onClick={toggleDrawer}>
+                <button className="m-4" onClick={toggleDrawer}>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
                          strokeLinecap="round" strokeLinejoin="round">
                         <line x1="7" y1="12" x2="20" y2="12"></line>
                         <line x1="7" y1="7" x2="20" y2="7"></line>
                         <line x1="7" y1="2" x2="20" y2="2"></line>
-
                     </svg>
-
                 </button>
                 <div className="sm:hidden"></div>
             </div>
@@ -37,10 +36,12 @@ const Home = () => {
                 <ChatList onSelectChat={setSelectedChatId} darkMode={darkMode}/>
                 {selectedChatId ? (
                     <ChatWindow chatId={selectedChatId.chatid} creator={selectedChatId.creator} darkMode={darkMode} onBack={onBack}/>
+
+
                 ) : (
                     <div
                         className={`flex-1 max-[400px]:hidden flex items-center justify-center ${darkMode ? 'bg-gray-900 border-gray-700 text-white' : 'bg-telegram-bg-main text-white border-gray-300'}`}>
-                        <span className={` ${darkMode? 'bg-slate-800' : 'bg-slate-700 opacity-90'} rounded-3xl  p-2 border-2`}>Select a chat to start messaging</span>
+                        <span className={` ${darkMode ? 'bg-slate-800' : 'bg-slate-700 opacity-90'} rounded-3xl  p-2 border-2`}>Select a chat to start messaging</span>
                     </div>
                 )}
             </div>
--- components/ChatWindow.js
+++ components/ChatWindow.js
@@ -1,10 +1,15 @@
+// ChatWindow.js
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import IconButton from '@mui/material/IconButton';
 
 const ChatWindow = ({ chatId, darkMode, creator, onBack }) => {
     const [messages, setMessages] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [newMessage, setNewMessage] = useState('');
     const chatContainerRef = useRef(null);
 
     useEffect(() => {
@@ -29,8 +34,14 @@ const ChatWindow = ({ chatId, darkMode, creator, onBack }) => {
         }
     }, [messages]);
 
+    const handleSendMessage = () => {
+        // Add functionality to send a message
+        setMessages([...messages, { id: Date.now(), message: newMessage, sender: { name: 'You' }, created_at: new Date().toISOString(), sender_id: 1 }]);
+        setNewMessage('');
+    };
+
     if (loading) {
-        return <div className={`p-4 ${darkMode ? 'text-telegram-dark-fg-main' : 'text-telegram-fg-main'}`}>Loading messages...</div>;
+        return <div className={`p-4 max-[400px]:hidden ${darkMode ? 'text-telegram-dark-fg-main' : 'text-telegram-fg-main'}`}>Loading messages...</div>;
     }
 
     if (error) {
@@ -38,7 +49,7 @@ const ChatWindow = ({ chatId, darkMode, creator, onBack }) => {
     }
 
     return (
-        <div ref={chatContainerRef} className={`flex-1  md:overflow-y-auto max-[400px]:absolute  h-screen ${darkMode ? 'bg-slate-900' : 'bg-gray-50'} flex-1   inset-0 z-10  ${(chatId && messages.length>0 ) ? 'translate-x-0' : '-translate-x-full'}  duration-100 ease-in-out`}>
+        <div ref={chatContainerRef} className={`flex flex-col h-screen max-[400px]:absolute  ${darkMode ? 'bg-slate-900' : 'bg-gray-50'} flex-1 inset-0 z-10 transform ${chatId ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out`}>
             <div className={`sticky top-0 mb-2 bg-inherit z-20 flex items-center justify-between p-4 border-b-1 border-black ${darkMode ? 'bg-slate-700' : 'bg-telegram-bg-main'}`}>
                 <button onClick={onBack} className="sm:hidden">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -50,7 +61,8 @@ const ChatWindow = ({ chatId, darkMode, creator, onBack }) => {
                 </h2>
                 <div className="sm:hidden"></div>
             </div>
-            <div className="flex flex-col space-y-4 mx-2  mb-24">
+            <div className={`flex flex-col space-y-4 px-2 flex-1 ${darkMode ? 'bg-slate-900' : 'bg-gray-50'}
+             overflow-y-auto overflow-x-clip [&::-webkit-scrollbar]:[width:4px] [&::-webkit-scrollbar-thumb]:bg-slate-500 [&::-webkit-scrollbar]:bg-slate-600 [&::-webkit-scrollbar-thumb]:rounded-full`} >
                 {messages.map(message => (
                     <div key={message.id} className={`p-2 rounded-lg max-w-[70%] ${
                         message.sender_id === 1
@@ -64,6 +76,36 @@ const ChatWindow = ({ chatId, darkMode, creator, onBack }) => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className={`sticky bottom-0 p-4 ${darkMode ? '' : 'bg-gray-100'} flex items-center`}>
+                <TextField
+                    variant="outlined"
+                    fullWidth
+                    placeholder="Type a message"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    InputProps={{
+                        style: {
+                            backgroundColor: darkMode ? '#232e3b' : '#fff',
+                            color: darkMode ? '#fff' : '#000',
+                            borderRadius: '25px',
+                        },
+                    }}
+                    sx={{
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none',
+                        },
+                        '&:hover .MuiOutlinedInput-notchedOutline': {
+                            border: 'none',
+                        },
+                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                            border: 'none',
+                        },
+                    }}
+                />
+                <IconButton color="primary" onClick={handleSendMessage}>
+                    <SendIcon />
+                </IconButton>
             </div>
         </div>
     );
